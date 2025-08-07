595b29423d5c689d19a4ffd835dc14fb646ae70e
Suleyman Farajli <suleyman@farajli.net>
2025-08-07 08:54:04 +0400

    feat(decision tree): classifier is set as a child class of base decision tree

--- prediction/decision_tree.py
+++ prediction/decision_tree.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 from collections import Counter
 
-# TODO: add regressor
+# FIXME: model gives different accuracy scores each time
 
 class Node:
     def __init__(self, right=None, left=None, threshold=None, feature=None, label=None):
@@ -18,10 +18,9 @@ class Node:
 
         return True
 
-class DecisionTreeClassifier:
+class BaseDecisionTree:
     def __init__(
         self,
-        criterion             = "gini", # entropy
         splitter              = "best", # random
         max_depth             = None,
         min_samples_split     = 2,
@@ -30,9 +29,7 @@ class DecisionTreeClassifier:
         random_state          = None,
         max_leaf_nodes        = None,
         min_impurity_decrease = 0.0,
-        class_weight          = None             # TODO: not implemented
     ):
-        self.criterion             = criterion
         self.splitter              = splitter
         self.max_depth             = max_depth
         self.min_samples_split     = min_samples_split
@@ -41,7 +38,6 @@ class DecisionTreeClassifier:
         self.random_state          = random_state
         self.max_leaf_nodes        = max_leaf_nodes
         self.min_impurity_decrease = min_impurity_decrease
-        self.class_weight          = class_weight
         self._root_node            = None
         self._leaf_count           = 0
 
@@ -60,22 +56,21 @@ class DecisionTreeClassifier:
         if self._impurity(y) == 0 or \
                 (self.max_depth is not None and depth >= self.max_depth) or \
                 len(y) < self.min_samples_split or \
-                # FIXME: doesn't work perfectly since leaf count can be one less than max leaf node
-                (self.max_leaf_nodes is not None and self._leaf_count + 1 > self.max_leaf_nodes):
-            node.label = self._getlabel(y)
+                (self.max_leaf_nodes is not None and self._leaf_count + 1 > self.max_leaf_nodes): # FIXME: doesn't work perfectly since leaf count can be one less than max leaf node
+            node.label = self._getvalue(y)
             self._leaf_count += 1
             return node
 
         feature_inx, threshold, info_gain = self._best_split_params(X, y)
         if feature_inx == None or threshold == None or info_gain <= self.min_impurity_decrease:
-            node.label = self._getlabel(y)
+            node.label = self._getvalue(y)
             self._leaf_count += 1
             return node
 
         X_right, X_left, y_right, y_left = self._split(X, y, feature_inx, threshold)
 
         if len(y_left) < self.min_samples_leaf or len(y_right) < self.min_samples_leaf:
-            node.label = self._getlabel(y)
+            node.label = self._getvalue(y)
             self._leaf_count += 1
             return node
 
@@ -105,42 +100,6 @@ class DecisionTreeClassifier:
 
         return info_gain
 
-    def _impurity(self, y):
-        if len(y) == 0:
-            return 0
-
-        if self.criterion == 'gini':
-            return self._gini_impurity(y)
-        elif self.criterion == 'entropy':
-            return self._entropy(y)
-
-    def _entropy(self, y):
-        all_samples = len(y)
-        entropy = 0
-
-        counts = Counter(y).values()
-
-        for count in counts:
-            probability = count / all_samples
-            if probability > 0:
-                entropy -= probability * np.log2(probability)
-
-        return entropy
-
-    def _getlabel(self, y):
-        return Counter(y).most_common(1)[0][0]
-
-
-    def _gini_impurity(self, y):
-        all_samples = len(y)
-        impurity = 1
-
-        counts = Counter(y).values()
-        for count in counts:
-            probability = count / all_samples
-            impurity -= probability**2
-
-        return impurity
 
     def _split(self, X, y, feature_inx, threshold):
         feature_column = X[:, feature_inx]
@@ -197,3 +156,52 @@ class DecisionTreeClassifier:
             return self._predict(x, node.right)
 
         return self._predict(x, node.left)
+
+    # Implemented in subclasses
+    def _impurity(self, y):
+        raise NotImplementedError("Use Classifier or Regressor")
+
+    def _getvalue(self, y):
+        raise NotImplementedError("Use Classifier or Regressor")
+
+class DecisionTreeClassifier(BaseDecisionTree):
+    def __init__(self, criterion = "gini", class_weight = None, **kwargs):
+        self.criterion = criterion
+        self.class_weight = class_weight
+        super().__init__(**kwargs)
+
+    def _impurity(self, y):
+        if len(y) == 0:
+            return 0
+
+        if self.criterion == 'gini':
+            return self._gini_impurity(y)
+        elif self.criterion == 'entropy':
+            return self._entropy(y)
+
+    def _entropy(self, y):
+        all_samples = len(y)
+        entropy = 0
+
+        counts = Counter(y).values()
+
+        for count in counts:
+            probability = count / all_samples
+            if probability > 0:
+                entropy -= probability * np.log2(probability)
+
+        return entropy
+
+    def _getvalue(self, y):
+        return Counter(y).most_common(1)[0][0]
+
+    def _gini_impurity(self, y):
+        all_samples = len(y)
+        impurity = 1
+
+        counts = Counter(y).values()
+        for count in counts:
+            probability = count / all_samples
+            impurity -= probability**2
+
+        return impurity
